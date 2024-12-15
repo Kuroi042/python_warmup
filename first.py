503e0c9aff0b3f0b1bb9d0bc41333a44332c6e45
MedCharaf <bouderraoui45@gmail.com>
2024-12-15 20:25:45 +0100

    warmup

---
+++ first.py
@@ -0,0 +1,19 @@
+def my_var():
+    num = 42
+    number =  "42"
+    var = "quarante-deux "
+    float_var = 42.0
+    bolvar =  True
+    listvar = [42,"quarante-deux"]
+    mylist = []
+    print(str(num) +" is type = "+ str(type(num)))
+    print(number +" is type = "+ str(type(number)))
+    print(var +" is type = "+str( type(var)))
+    print(str(float_var)+" is type = "+ str(type(float_var)))
+
+    return 
+
+
+
+if __name__ == '__main__':
+    my_var()
