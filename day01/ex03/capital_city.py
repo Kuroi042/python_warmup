fcd1df176b59cdc458096929e3cd14e365b1a8b0
MedCharaf <bouderraoui45@gmail.com>
2024-12-17 22:47:30 +0100

    mab9atch rwina ajemi

--- day01/ex03/capital_city.py
+++ day01/ex03/capital_city.py
@@ -13,11 +13,28 @@ capital_cities = {
 }
 
 def printarg():
+    if(len(sys.argv)!=2):
+        print("args error\n")
+        return
+    else :
+        state_name  = sys.argv[1]
+        state_abv =  states.get(state_name)
+        if(state_abv in capital_cities ):
+            print(capital_cities.get(state_abv))
+
+
+
+
+if __name__ == '__main__':
+    printarg()
+
+
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+def printarg():
     if(len(sys.argv)==2 ):
         if(  sys.argv[1] in states):
-            # print(str(sys.argv[1]))
-            # print(states[sys.argv[1]])
-        # for keyscity, valuecity  in capital_cities.items():
+            print(str(sys.argv[1]))
             if( states[sys.argv[1]] in capital_cities ):
                 print(capital_cities[states[sys.argv[1]]]) 
             
