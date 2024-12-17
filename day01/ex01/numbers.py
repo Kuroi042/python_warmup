8482759a7e9fa86dcbf1770aa62d9de71ff5eb13
MedCharaf <bouderraoui45@gmail.com>
2024-12-17 16:45:49 +0100

    ex01

--- day01/ex01/numbers.py
+++ day01/ex01/numbers.py
@@ -1,21 +1,18 @@
 def openfile():
-    f  = open("text.txt" ,"r")
-    return f
-def printfile():
-    f  = openfile()
-    zab = []
-    for i in range(len(f)):
-        zab.append((a[i]))
-
-
-
+    with open("text.txt" ,"r") as f:
+        data =  f.read()
+    return data
+def splitelement():
+    f =  openfile()
+    elements = f.split(",")
+    return elements
+def printelements():
+    elements = splitelement()
+    for numbers in  elements:
+        print(numbers.strip() )
     return
 
 
 
 if __name__ == '__main__':
-    printfile()
-    # with f as zabi:
-    #     for line in zabi:
-    #         x =line.split(',')
-    #         print(x)
+    printelements()
