94cfdfa83f963b959b92b2bd60f2fd2d98aacf26
MedCharaf <bouderraoui45@gmail.com>
2024-12-17 01:48:00 +0100

    mellali

---
+++ day01/ex01/numbers.py
@@ -0,0 +1,21 @@
+def openfile():
+    f  = open("text.txt" ,"r")
+    return f
+def printfile():
+    f  = openfile()
+    zab = []
+    for i in range(len(f)):
+        zab.append((a[i]))
+
+
+
+    return
+
+
+
+if __name__ == '__main__':
+    printfile()
+    # with f as zabi:
+    #     for line in zabi:
+    #         x =line.split(',')
+    #         print(x)
