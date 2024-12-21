6b113b9f749f2432775f0f262756ddc2ed20a1b6
Mohamed charaf Bouderraoui <mbouderr@c2r3p8.1337.ma>
2024-12-21 01:00:05 +0100

    mext

--- day02/ex01/intern.py
+++ day02/ex01/intern.py
@@ -2,7 +2,9 @@ class Intern:
     def __init__(self,name = "My name? I’m nobody, an intern, I have no name.", time = 0 ):
         self.name = name
         self.time  = time
-    
+    def __str__(self):
+        return f"{self.name}"
+         
     def Coffee(self):
             print("This is the worst coffee you ever tasted.")
     def work(self):
@@ -10,17 +12,21 @@ class Intern:
                 raise Exception ("ana gha wliya") 
             print("Sorry im just an intern khassni nmchi fhali")
 
-    def __str__(self):
-        return f"{self.name }"
 
-charaf =  Intern()
-# charaf.Coffee()
-try:
+# randIterm =  Intern()
+
+# # charaf.Coffee()
+# try:
+#     randIterm.work()
+# except Exception as e:
+#      print(e)
+
+charaf = Intern()
+print(charaf)
+charaf.Coffee()
+try :
     charaf.work()
 except Exception as e:
      print(e)
-# print(charaf.name , charaf.age)
-
-
 
 
